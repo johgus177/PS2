d05bcea7fd7495818294a0d7bb0e03649c599c81
johgus177 <31348516+johgus177@users.noreply.github.com>
2017-08-25 16:05:30 +0200

    Revert "lagt till mer text"
    This reverts commit 490814d1794f200fa98b32cbbf4be34c0db30076.

--- comparetxt.ps1
+++ comparetxt.ps1
@@ -4,3 +4,8 @@ $A = Get-Content C:\patch\a.txt
 $B = Get-Content C:\patch\b.txt
 
 Compare-Object -referenceObject $a -differenceObject $b
+<<<<<<< HEAD
+=======
+
+text
+>>>>>>> parent of 490814d... lagt till mer text
