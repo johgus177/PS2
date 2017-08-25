f9658aaddded55b924b29b0b50373a95a7358441
johgus177 <31348516+johgus177@users.noreply.github.com>
2017-08-25 16:11:47 +0200

    Revert "Original"
    This reverts commit 3cb1e60a385cdb713113c5cc5358e7654a2d120f.

--- comparetxt.ps1
+++ comparetxt.ps1
@@ -3,9 +3,4 @@ Clear-Host
 $A = Get-Content C:\patch\a.txt
 $B = Get-Content C:\patch\b.txt
 
-Compare-Object -referenceObject $a -differenceObject $b
-<<<<<<< HEAD
-=======
-
-text
->>>>>>> parent of 490814d... lagt till mer text
+Compare-Object -referenceObject $a -differenceObject $b | Out-GridView
